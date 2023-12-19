9b09daebc1c56e9ee9fc7ed11a62af622ebcb51a
Auric  Mondal <67121184+Auricmondal@users.noreply.github.com>
2023-12-19 08:46:25 +0530

    Update page.js
    optimised

--- app/page.js
+++ app/page.js
@@ -1,12 +1,53 @@
 import Image from 'next/image'
 import Banner from './components/Banner'
 import FeaturedCard from './components/FeaturedCard'
+import Section from "./components/Section";
+import InnerSection from "./components/InnerSection";
+import GridSystem from "./components/featured/GridSystem";
 
 export default function Home() {
   return (
     <div>
       <Banner/>
-      <FeaturedCard/>
+      <Section>
+        <h2
+          className="
+        text-center 
+        md:text-left 
+        font-extrabold 
+        text-4xl 
+        lg:text-5xl
+        "
+        >
+          Featured
+        </h2>
+        <InnerSection>
+          <GridSystem>
+            <FeaturedCard 
+              action={'/'}
+              type={'2r2c'}
+              label={'Maps'}
+              imageUrl={`bg-[url('/assets/images/map.png')]`}
+            />
+            <FeaturedCard 
+              action={'/'}
+              type={'2r'}
+              label={'Weather'}
+              imageUrl={`bg-[url('/assets/images/Weather.png')]`}
+            />
+            <FeaturedCard 
+              action={'/'}
+              label={'Data Archives'}
+              imageUrl={`bg-[url('/assets/images/da.png')]`}
+            />
+            <FeaturedCard 
+              action={'/'}
+              label={'Vista'}
+              imageUrl={`bg-[url('/assets/images/vista.png')]`}
+            />
+          </GridSystem>
+        </InnerSection>
+      </Section>
     </div>
   )
 }
