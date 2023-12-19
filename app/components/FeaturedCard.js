a97cedaa722d1ad131500dc870bae65a498ab89d
Auric  Mondal <67121184+Auricmondal@users.noreply.github.com>
2023-12-19 08:45:13 +0530

    Update FeaturedCard.js
    optimised

--- app/components/FeaturedCard.js
+++ app/components/FeaturedCard.js
@@ -1,29 +1,37 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
-import Image from 'next/image'
-const FeaturedCard = () => {
+const FeaturedCard = ({
+    label,
+    imageUrl,
+    type,
+    action
+}) => {
+    const router =useRouter();
   return (
-    <div>
-        <div className=' py-4 text-center md:text-left md:ml-12 font-extrabold text-5xl'>Featured</div>
 
-        <div className=' bg-white ml-5 md:ml-9 md:h-[1150] md:flex items-center'>
-            <div className=' bg-red-400 mb-4 ml-4 h-[390px] md:h-[390px] w-[420px] md:w-[720px] place-content-center flex-row items-center rounded-md overflow-hidden shadow-lg text-black'>
-                <p className=' mt-40 md:mt-44 font-bold text-4xl text-center mb-2'>Map</p>
+        <div 
+        className={`
+         ${imageUrl}
+         bg-cover
+         ${type=='2r2c'? 'md:col-span-2 row-span-2':type=='2r'?'row-span-2':''}
+         text-center
+         min-h-[190px]
+         flex
+         items-center
+         justify-center
+         shadow-lg 
+         relative
+         cursor-pointer
+         `}
+         onClick={()=>{router.push(action)}}
+         >
+            <div className='absolute opacity-40 bg-black h-full w-full'></div>
+                <p className='z-10 text-white font-bold text-4xl text-center'>{label}</p>
             </div>
-            
-            <div className=' bg-red-400 mb-4 ml-4 h-[398px] md:h-[390px] w-[420px] md:w-[310px] place-content-center flex-row items-center rounded-md overflow-hidden shadow-lg text-black'>
-                <p className=' mt-40 md:mt-44 font-bold text-4xl text-center mb-2'>Weather</p>
-            </div>
-            <div className=' flex-row'>
-                <div className=' bg-red-400 md:mb-2 md:mr-9 mb-4 ml-4 h-[196px] md:h-[190px] w-[420px] md:w-[370px] place-content-start flex-col items-center rounded-md overflow-hidden shadow-lg text-black'>
-                    <p className=' mt-20 md:mt-20 font-bold text-4xl text-center mb-2'>Data Archives</p>
-                </div>
-                <div className=' bg-red-400 md:mr-9 mb-4 ml-4 h-[203px] md:h-[190px] w-[420px] md:w-[370px] place-content-center flex-col items-center rounded-md overflow-hidden shadow-lg text-black'>
-                <p className=' mt-20 md:mt-16 font-bold text-4xl text-center mb-2'>Vista</p>
-                </div>
-            </div>
-        </div>
-    </div>
+        
     
+
   )
 }
 
